9aca0fdc93d66f1acf11086ef30326fa10966696
MMunim <mmunim9.9.01@gmail.com>
2025-05-24 00:03:29 +0600

    browse listing section complete

--- src/Page/BrowseListing.jsx
+++ src/Page/BrowseListing.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { FaArrowLeftLong } from "react-icons/fa6";
 import { Link, useLoaderData } from "react-router";
+import AdCard from "../components/AdCard";
 
-const BrowseListing = ({ ad }) => {
-// const allAds = useLoaderData();
+const BrowseListing = () => {
+const allAds = useLoaderData();
   return (
     <div>
-      <div className="font-bold text-xl py-6 text-center">
-        <div className="btn border-2 border-black text-white bg-secondary text-xl">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-10">
+          {allAds.map((ad) => (
+              <AdCard key={ad._id} ad={ad}></AdCard>
+            ))}
+        </div>
+      <div className="font-bold text-xl py-10 text-center">
+        <div className="btn border-2 border-black text-xl">
           <FaArrowLeftLong className="inline" /> &nbsp;
           <Link to="/">Back to home</Link>
         </div>
