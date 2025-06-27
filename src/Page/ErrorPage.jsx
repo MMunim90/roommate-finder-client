ba698af6507b74fa04d943d6b1e9040dd6a1673b
MMunim <mmunim9.9.01@gmail.com>
2025-06-27 15:08:28 +0600

    redesign the error page

--- src/Page/ErrorPage.jsx
+++ src/Page/ErrorPage.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { FaArrowLeftLong } from "react-icons/fa6";
-import { Link } from "react-router";
+import { Link, useRouteError } from "react-router";
 
 const ErrorPage = () => {
+  const error = useRouteError();
   return (
-    <div className="w-11/12 flex flex-col justify-center items-center">
-    <img src="https://i.ibb.co/v4THdMnz/404.gif" alt="" />
-    <button className="btn font-bold text-xl py-6 text-center">
+    <div className="w-11/12 mx-auto flex flex-col justify-center items-center bg-white p-16">
+      <img src="https://i.ibb.co/v4THdMnz/404.gif" alt="" />
+      <h1 className="text-red-600 font-bold text-4xl mb-4">{error?.status}</h1>
+      <p className="mb-5 text-xl font-bold text-red-600 md:text-2xl">
+        {error?.error?.message || "Something Went Wrong!"}
+      </p>
+      <button className="btn font-bold text-xl py-6 text-center">
         <FaArrowLeftLong className="inline" /> &nbsp;
         <Link to="/">Back to home</Link>
       </button>
