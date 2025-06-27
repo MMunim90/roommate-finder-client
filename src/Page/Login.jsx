6100aab8051f3ec33e9dfbd046a380de9f19f1ab
MMunim <mmunim9.9.01@gmail.com>
2025-06-27 14:55:50 +0600

    added lottie on login and register page

--- src/Page/Login.jsx
+++ src/Page/Login.jsx
@@ -5,6 +5,9 @@ import { AuthContext } from "../Provider/AuthProvider";
 import { toast } from "react-toastify";
 import Swal from "sweetalert2";
 
+import loginLottie from "../../src/assets/lottie/login.json";
+import Lottie from "lottie-react";
+
 const Login = () => {
   const [error, setError] = useState("");
   const location = useLocation();
@@ -61,66 +64,67 @@ const Login = () => {
   };
 
   return (
-    <div className="w-full max-w-md p-8 space-y-3 rounded-xl bg-gray-400 my-10 mx-auto">
-      <h1 className="text-2xl font-bold text-center">Login</h1>
-      <form onSubmit={handleLogin} className="space-y-6">
-        <div className="space-y-1 text-sm">
-          <label className="block text-gray-600">Email</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="Email"
-            required
-            className="w-full px-4 py-3 rounded-md border-gray-300 bg-gray-50 text-gray-800"
-          />
-        </div>
-        <div className="space-y-1 text-sm">
-          <label htmlFor="password" className="block dark:text-gray-600">
-            Password
-          </label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Password"
-            required
-            className="w-full px-4 py-3 rounded-md border-gray-300 bg-gray-50 text-gray-800"
-          />
-          {/* <div className="flex justify-end text-xs text-gray-600 hover:underline">
-            <Link to="/auth/forgetPass" rel="noopener noreferrer">
-              Forgot Password?
-            </Link>
-          </div> */}
+    <div className="hero-content flex-col lg:flex-row">
+      <div className="text-center lg:text-left w-auto ">
+        <Lottie animationData={loginLottie} loop={true}></Lottie>
+      </div>
 
-          {error && <p className="text-red-600 text-xs">{error}</p>}
+      <div className="w-full max-w-md p-8 space-y-3 rounded-xl bg-gray-400 lg:my-10 mx-auto">
+        <h1 className="text-2xl font-bold text-center">Login</h1>
+        <form onSubmit={handleLogin} className="space-y-6">
+          <div className="space-y-1 text-sm">
+            <label className="block text-gray-600">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email"
+              required
+              className="w-full px-4 py-3 rounded-md border-gray-300 bg-gray-50 text-gray-800"
+            />
+          </div>
+          <div className="space-y-1 text-sm">
+            <label htmlFor="password" className="block dark:text-gray-600">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+              required
+              className="w-full px-4 py-3 rounded-md border-gray-300 bg-gray-50 text-gray-800"
+            />
+
+            {error && <p className="text-red-600 text-xs">{error}</p>}
+          </div>
+          <button
+            type="submit"
+            className="block w-full p-3 text-center text-white text-2xl font-bold rounded-sm bg-black cursor-pointer"
+          >
+            Log in
+          </button>
+        </form>
+        <div className="flex items-center pt-4 space-x-1">
+          <div className="flex-1 h-px sm:w-16 dark:bg-gray-300"></div>
+          <p className="px-3 text-2xl font-bold">OR</p>
+          <div className="flex-1 h-px sm:w-16 dark:bg-gray-300"></div>
         </div>
-        <button
-          type="submit"
-          className="block w-full p-3 text-center text-white text-2xl font-bold rounded-sm bg-black cursor-pointer"
-        >
-          Log in
-        </button>
-      </form>
-      <div className="flex items-center pt-4 space-x-1">
-        <div className="flex-1 h-px sm:w-16 dark:bg-gray-300"></div>
-        <p className="px-3 text-2xl font-bold">OR</p>
-        <div className="flex-1 h-px sm:w-16 dark:bg-gray-300"></div>
+        <div className="flex justify-center space-x-4 my-6">
+          <button
+            onClick={handleGoogleSignIn}
+            className="btn btn-outline text-xl  w-full"
+          >
+            <FcGoogle size={24} /> Login With Google
+          </button>
+        </div>
+        <p className="text-md text-center sm:px-6">
+          Don't have an account?
+          <Link to="/register" className="ml-2 underline text-black">
+            Register
+          </Link>
+        </p>
       </div>
-      <div className="flex justify-center space-x-4 my-6">
-        <button
-          onClick={handleGoogleSignIn}
-          className="btn btn-outline text-xl  w-full"
-        >
-          <FcGoogle size={24} /> Login With Google
-        </button>
-      </div>
-      <p className="text-md text-center sm:px-6">
-        Don't have an account?
-        <Link to="/register" className="ml-2 underline text-black">
-          Register
-        </Link>
-      </p>
     </div>
   );
 };
--- src/Page/Register.jsx
+++ src/Page/Register.jsx
@@ -7,6 +7,9 @@ import { AuthContext } from "../Provider/AuthProvider";
 import { toast } from "react-toastify";
 import Swal from "sweetalert2";
 
+import loginLottie from "../../src/assets/lottie/register.json";
+import Lottie from "lottie-react";
+
 const Register = () => {
   const { createUser, setUser, updateUserProfile, signInWithGoogle } =
     use(AuthContext);
@@ -31,7 +34,7 @@ const Register = () => {
       setPasswordError("at least one uppercase.");
       return;
     } else {
-        setPasswordError("");
+      setPasswordError("");
     }
 
     createUser(email, password)
@@ -39,10 +42,10 @@ const Register = () => {
         const user = result.user;
         // console.log(user)
         Swal.fire({
-            title: "Welcome!",
-            text: "user created successfully!",
-            icon: "success"
-          });
+          title: "Welcome!",
+          text: "user created successfully!",
+          icon: "success",
+        });
         navigate("/");
         updateUserProfile({
           displayName: name,
@@ -72,10 +75,10 @@ const Register = () => {
         const user = result.user;
         navigate("/");
         Swal.fire({
-                  title: `${user.displayName} SignUp successful`,
-                  icon: "success",
-                  draggable: true,
-                });
+          title: `${user.displayName} SignUp successful`,
+          icon: "success",
+          draggable: true,
+        });
       })
       .catch((error) => {
         const errorCode = error.code;
@@ -86,113 +89,118 @@ const Register = () => {
   };
 
   useEffect(() => {
-  if (passwordError) {
-    Swal.fire({
-      title: passwordError,
-      icon: "error",
-      draggable: true,
-    }).then(() => {
-      // Clear error after clicking OK
-      setPasswordError(""); // You must define setPasswordError from useState
-    });
-  }
-}, [passwordError]);
+    if (passwordError) {
+      Swal.fire({
+        title: passwordError,
+        icon: "error",
+        draggable: true,
+      }).then(() => {
+        // Clear error after clicking OK
+        setPasswordError(""); // You must define setPasswordError from useState
+      });
+    }
+  }, [passwordError]);
   return (
-    <div className="w-full max-w-md p-8 space-y-3 rounded-xl bg-gray-400 mx-auto my-10">
-    
-      <h1 className="text-2xl font-bold text-center">Register</h1>
-      <form onSubmit={handleRegister} className="space-y-6">
-        <div className="space-y-1 text-sm">
-          <label htmlFor="username" className="block dark:text-gray-600">
-            Username
-          </label>
-          <input
-            type="text"
-            name="name"
-            id="username"
-            placeholder="Username"
-            required
-            className="w-full px-4 py-3 rounded-md border-gray-300 bg-gray-50 text-gray-800"
-          />
-        </div>
+    <div className="hero-content flex-col lg:flex-row">
+      <div className="text-center lg:text-left w-[250px] md:w-[400px] lg:w-[600px]">
+        <Lottie animationData={loginLottie} loop={true}></Lottie>
+      </div>
 
-        <div className="space-y-1 text-sm">
-          <label className="block text-gray-600">Email</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="Email"
-            required
-            className="w-full px-4 py-3 rounded-md border-gray-300 bg-gray-50 text-gray-800"
-          />
-        </div>
-
-        <div className="space-y-1 text-sm">
-          <label className="block text-gray-600">Photo URL</label>
-          <input
-            type="text"
-            name="photoURL"
-            id="photoURL"
-            placeholder="Photo URL"
-            required
-            className="w-full px-4 py-3 rounded-md border-gray-300 bg-gray-50 text-gray-800"
-          />
-        </div>
-
-        <div className="space-y-1 text-sm">
-          <label htmlFor="password" className="block dark:text-gray-600">
-            Password
-          </label>
-          <div className="relative">
+      <div className="w-full max-w-md p-8 space-y-3 rounded-xl bg-gray-400 mx-auto my-10">
+        <h1 className="text-2xl font-bold text-center">Register</h1>
+        <form onSubmit={handleRegister} className="space-y-6">
+          <div className="space-y-1 text-sm">
+            <label htmlFor="username" className="block dark:text-gray-600">
+              Username
+            </label>
             <input
-              type={showPassword ? "text" : "password"}
-              name="password"
-              id="password"
-              placeholder="Password"
+              type="text"
+              name="name"
+              id="username"
+              placeholder="Username"
               required
               className="w-full px-4 py-3 rounded-md border-gray-300 bg-gray-50 text-gray-800"
             />
-            <button
-              onClick={() => {
-                setShowPassword(!showPassword);
-              }}
-              className="btn btn-xs absolute top-[10px] right-4 bg-gray-400"
-            >
-              {showPassword ? <FaEyeSlash size={16}/> : <FaEye size={16}/>}
-            </button>
           </div>
 
-          {passwordError && (
-            <p className="text-red-600 text-xs">{passwordError}</p>
-          )}
+          <div className="space-y-1 text-sm">
+            <label className="block text-gray-600">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email"
+              required
+              className="w-full px-4 py-3 rounded-md border-gray-300 bg-gray-50 text-gray-800"
+            />
+          </div>
+
+          <div className="space-y-1 text-sm">
+            <label className="block text-gray-600">Photo URL</label>
+            <input
+              type="text"
+              name="photoURL"
+              id="photoURL"
+              placeholder="Photo URL"
+              required
+              className="w-full px-4 py-3 rounded-md border-gray-300 bg-gray-50 text-gray-800"
+            />
+          </div>
+
+          <div className="space-y-1 text-sm">
+            <label htmlFor="password" className="block dark:text-gray-600">
+              Password
+            </label>
+            <div className="relative">
+              <input
+                type={showPassword ? "text" : "password"}
+                name="password"
+                id="password"
+                placeholder="Password"
+                required
+                className="w-full px-4 py-3 rounded-md border-gray-300 bg-gray-50 text-gray-800"
+              />
+              <button
+                onClick={() => {
+                  setShowPassword(!showPassword);
+                }}
+                className="btn btn-xs absolute top-[10px] right-4 bg-gray-400"
+              >
+                {showPassword ? <FaEyeSlash size={16} /> : <FaEye size={16} />}
+              </button>
+            </div>
+
+            {passwordError && (
+              <p className="text-red-600 text-xs">{passwordError}</p>
+            )}
+          </div>
+          <button
+            type="submit"
+            className="block w-full p-3 text-center text-white text-2xl font-bold rounded-sm bg-black cursor-pointer"
+          >
+            Register
+          </button>
+        </form>
+        <div className="flex items-center pt-4 space-x-1">
+          <div className="flex-1 h-px sm:w-16 dark:bg-gray-300"></div>
+          <p className="px-3 text-2xl font-bold">OR</p>
+          <div className="flex-1 h-px sm:w-16 dark:bg-gray-300"></div>
         </div>
-        <button
-          type="submit"
-          className="block w-full p-3 text-center text-white text-2xl font-bold rounded-sm bg-black cursor-pointer"
-        >
-          Register
-        </button>
-      </form>
-      <div className="flex items-center pt-4 space-x-1">
-        <div className="flex-1 h-px sm:w-16 dark:bg-gray-300"></div>
-        <p className="px-3 text-2xl font-bold">OR</p>
-        <div className="flex-1 h-px sm:w-16 dark:bg-gray-300"></div>
+        <div className="flex justify-center space-x-4 my-6">
+          <button
+            onClick={handleGoogleSignIn}
+            className="btn btn-outline text-xl w-full p-8 md:p-0"
+          >
+            <FcGoogle size={24} /> Register With Google
+          </button>
+        </div>
+        <p className="text-md text-center sm:px-6 dark:text-gray-600">
+          Already have an account?
+          <Link to="/login" className="ml-2 underline text-black">
+            Log in
+          </Link>
+        </p>
       </div>
-      <div className="flex justify-center space-x-4 my-6">
-        <button
-          onClick={handleGoogleSignIn}
-          className="btn btn-outline text-xl w-full"
-        >
-          <FcGoogle size={24} /> Register With Google
-        </button>
-      </div>
-      <p className="text-md text-center sm:px-6 dark:text-gray-600">
-        Already have an account?
-        <Link to="/login" className="ml-2 underline text-black">
-          Log in
-        </Link>
-      </p>
     </div>
   );
 };
